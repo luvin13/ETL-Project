4a83722e413ad1e20058019ae20a1d8122911034
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-06-05 10:50:34 -0700

    Started world happy

--- schema.sql
+++ schema.sql
@@ -1,17 +1,10 @@
 DROP TABLE IF EXISTS multihappy;
 DROP TABLE IF EXISTS happy2021;
 
-CREATE TABLE multihappy (
-    country_name VARCHAR(30) NOT NULL,
-    year INT NOT NULL,
-    life_ladder DEC NOT NULL,
-    PRIMARY KEY (country_name)
-);
-
-CREATE TABLE happy2021 (
-    country_name VARCHAR(30) NOT NULL,
+CREATE TABLE worldhappy (
+    country_name VARCHAR(30) NOT NULL,   
     regional_indicator VARCHAR(30) NOT NULL,
+    year INT NOT NULL,
     ladder_score DEC NOT NULL,
-    PRIMARY KEY (country_name),
-    FOREIGN KEY (country_name) REFERENCES multihappy(country_name)
+    PRIMARY KEY (country_name)
 );
